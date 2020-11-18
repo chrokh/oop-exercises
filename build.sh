1fa27549434f3c16212a2be855978adbf49ddfd5
chrokh <christopher.okhravi@gmail.com>
2020-11-18 14:28:32 +0100

    Custom served folder must apparently be named 'docs'.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mkdir -p dist/static
-mkdir -p dist/static/examples
-mkdir -p dist/static/exercises
-cp src/static/style.css dist/static/style.css
-pandoc -s -c "static/style.css" src/index.md -o dist/index.html
-pandoc -s -c "../static/style.css" src/exercises/index.md -o dist/exercises/index.html
-pandoc -s -c "../static/style.css" src/examples/command-line-args-and-object-composition.md -o dist/examples/command-line-args-and-object-composition.html
+mkdir -p docs/static
+mkdir -p docs/examples
+mkdir -p docs/exercises
+cp src/static/style.css docs/static/style.css
+pandoc -s -c "static/style.css" src/index.md -o docs/index.html
+pandoc -s -c "../static/style.css" src/exercises/index.md -o docs/exercises/index.html
+pandoc -s -c "../static/style.css" src/examples/command-line-args-and-object-composition.md -o docs/examples/command-line-args-and-object-composition.html
